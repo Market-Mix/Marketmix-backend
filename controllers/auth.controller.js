0b31b5b8aa595471ca7356c67daaf3ec595bd6cd
simon <maxsimon790@gmail.com>
2025-12-04 23:56:21 +0100

    G

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -379,7 +379,7 @@ const getMe = async (req, res) => {
         id: user.id,
         email: user.email,
         firstName: user.first_name,
-        lastName: user.last_namegit ,
+        lastName: user.last_name,
         phone: user.phone,
         role: user.role,
         avatar_url: user.avatar_url,
