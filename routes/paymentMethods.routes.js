1315b4d8218d4b7e1339d2e5ee7802b656d40cdf
simon <maxsimon790@gmail.com>
2025-12-06 23:55:21 +0100

    hh

--- routes/paymentMethods.routes.js
+++ routes/paymentMethods.routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const paymentMethodsController = require('../controllers/paymentMethods.controller');
-const { protect } = require('../middlewares/auth.middleware');
- // your actual auth
+const { protect } = require('../middlewares/auth.middleware'); // <-- your JWT auth middleware
 
 // Apply authentication middleware to all routes
 router.use(protect);
@@ -13,7 +12,7 @@ router.get('/default', paymentMethodsController.getDefaultPaymentMethod);
 router.get('/:id', paymentMethodsController.getPaymentMethodById);
 router.post('/', paymentMethodsController.createPaymentMethod);
 router.put('/:id', paymentMethodsController.updatePaymentMethod);
-router.put('/:id/set-default', paymentMethodsController.setDefaultPaymentMethod);
+router.put('/:id/set-default', paymentMethodsController.updatePaymentMethod); // <-- use correct function
 router.delete('/:id', paymentMethodsController.deletePaymentMethod);
 
 module.exports = router;
