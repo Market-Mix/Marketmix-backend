c53e0ff3e48e2143924a202225adb975ea8dc826
emele <eemele75@gmail.com>
2025-12-12 06:02:52 -0800

    pokijuhyu

--- routes/wishlist.routes.js
+++ routes/wishlist.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const wishlist = require('../controllers/wishlist.controller');
-const { authenticate } = require('../middlewares/auth.middleware');
+const { protect } = require('../middlewares/auth.middleware');
 
-router.post('/add', authenticate, wishlist.addToWishlist);
-router.get('/', authenticate, wishlist.getWishlist);
+router.post('/add', protect, wishlist.addToWishlist);
+router.get('/', protect, wishlist.getWishlist);
 
 module.exports = router;
