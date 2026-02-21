d029d08b96b4349a4ebcb41ae7f5e4fcba3c186b
simon <maxsimon790@gmail.com>
2026-02-21 08:17:36 +0000

    HH

--- routes/seller.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Seller routes placeholder
-router.get('/', (req, res) => {
-	res.json({ status: 'success', message: 'Seller routes placeholder' });
-});
-
-module.exports = router;
